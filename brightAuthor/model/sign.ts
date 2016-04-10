d288f3ccf06dfc4e81047199083ffa350a3670a2
Ted Shaffer <ted@brightsign.biz>
2016-04-10 08:48:10 -0700

    initial zone parsing

--- brightAuthor/model/sign.ts
+++ brightAuthor/model/sign.ts
@@ -6,9 +6,8 @@ class Sign {
     name: string;
     model: string;
     videoMode: string;
+    zoneList: Object[];
 
-    // zoneList: Object[];
-    
     // constructor(name: string) {
     //     this.name = name;
     // }
@@ -21,12 +20,20 @@ class Sign {
 
         let sign = signAsJSON.BrightAuthor;
         let signMetadata = sign.meta[0];
-        // this.zoneList = sign.zones;
 
         this.name = signMetadata.name[0];
         this.model = signMetadata.model[0];
         this.videoMode = signMetadata.videoMode[0];
-    }
+        this.zoneList = [];
 
+        let self = this;
+        // TODO correct parsing for multiple zones
+        sign.zones.forEach( function(zoneAsJSONArray){
+            let zoneAsJSON = zoneAsJSONArray.zone[0];
+            var zone = new Zone();
+            zone.parse(zoneAsJSON);
+            self.zoneList.push(zone);
+        });
+    }
 }
 
---
+++ brightAuthor/model/zones/zone.ts
@@ -0,0 +1,26 @@
+/**
+ * Created by tedshaffer on 4/10/16.
+ */
+class Zone {
+
+    name: string;
+
+    // constructor(name: string) {
+    //     this.name = name;
+    // }
+
+    constructor() {
+        this.name = "";
+    }
+
+    parse(zoneAsJSON) {
+
+        console.log("pizzza");
+        
+        let zoneAsMetadata = zoneAsJSON;
+
+        this.name = zoneAsMetadata.name[0];
+
+    }
+
+}
